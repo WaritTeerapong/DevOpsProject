95b344ebd4ed8923070af7a42da2ea1cd8c35eaf
WaritTeerapong <smart29952@gmail.com>
2026-05-19 19:07:11 +0700

    chore: update .gitignore and sync workspace state

--- src/lib/hash.ts
+++
@@ -1,9 +0,0 @@
-import crypto from 'crypto';
-
-export const hashFlag = (flag: string) => {
-  return crypto.createHash('sha256').update(flag).digest('hex');
-};
-
-export const hashPassword = (password: string) => {
-    return crypto.createHash('sha256').update(password).digest('hex');
-};
